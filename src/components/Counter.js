ac5abacd768697dd47ca92a279d837ea9b6bb971
Léa LEE <lealee@MacBook-Pro-de-Lea.local>
2022-07-11 17:48:59 +0200

    css added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,15 +3,60 @@ import React, { useState } from "react";
 const Counter = () => {
   const [counter, setCounter] = useState(0);
 
+  // const button = document.querySelector(".desc");
+  // button.disabled = true;
+  // function stateButton() {
+  //   if (button.value === 0) {
+  //     button.disabled = true;
+  //   } else {
+  //     button.disabled = false;
+  //   }
+  // }
+
   return (
     <div className="container">
-      <button onClick={() => setCounter((count) => count - 1)}>-</button>
-      <button type={"button"} onClick={() => setCounter((count) => count + 1)}>
-        +
-      </button>
+      <div className="counter-container">
+        <button
+          style={{
+            border: "none",
+            backgroundColor: "white",
+            fontSize: 80,
+            cursor: "pointer",
+          }}
+          className="desc"
+          onClick={() => setCounter((count) => count - 1)}
+        >
+          {/* count va update le state au nv state  */}-
+        </button>
+        <div className="counter">{counter}</div>
+        <button
+          style={{
+            border: "none",
+            backgroundColor: "white",
+            fontSize: 65,
+            cursor: "pointer",
+          }}
+          className="inc"
+          type={"button"}
+          onClick={() => setCounter((count) => count + 1)}
+        >
+          +
+        </button>
+      </div>
 
-      {counter}
-      <button type={"button"} onClick={() => setCounter(0)}>
+      <button
+        style={{
+          border: "none",
+          backgroundColor: "#9772FB",
+          width: 80,
+          height: 40,
+          borderRadius: 10,
+          cursor: "pointer",
+        }}
+        className="reset"
+        type={"button"}
+        onClick={() => setCounter(0)}
+      >
         reset
       </button>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,44 @@ import React from "react";
 
 function Header() {
   return (
-    <div>
+    <div className="header">
+      <svg
+        className="logos"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 256 256"
+      >
+        <rect width="256" height="256" fill="none" />
+        <rect
+          width="96"
+          height="48"
+          x="80"
+          y="64"
+          fill="none"
+          stroke="#000"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="16"
+        />
+        <rect
+          width="192"
+          height="160"
+          x="32"
+          y="48"
+          fill="none"
+          stroke="#000"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="16"
+          rx="8"
+          transform="rotate(90 128 128)"
+        />
+        <circle cx="88" cy="148" r="12" />
+        <circle cx="128" cy="148" r="12" />
+        <circle cx="168" cy="148" r="12" />
+        <circle cx="88" cy="188" r="12" />
+        <circle cx="128" cy="188" r="12" />
+        <circle cx="168" cy="188" r="12" />
+      </svg>
       <h1>React Counter</h1>
     </div>
   );
